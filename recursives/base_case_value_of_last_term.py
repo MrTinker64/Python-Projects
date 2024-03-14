7c6b0b822509b8063d482af8e69c7c192613131d
MrTinker64 <avi.micah@gmail.com>
2024-03-14 13:40:26 -0700

    Change termination condition in iterative_loop function

--- recursives/base_case_value_of_last_term.py
+++ recursives/base_case_value_of_last_term.py
@@ -5,8 +5,9 @@ sequence = []
 @lru_cache(maxsize=None)  # Unlimited cache
 def iterative_loop(n: int):
     sequence.append(round(n, 1))
-    if sequence[len(sequence) - 1] >= 985:
+    if sequence[len(sequence) - 1] >= 0:
         print(sequence)
+        print(f"Length: {len(sequence)}")
     else:
         iterative_loop(1.04*n+75)
     
