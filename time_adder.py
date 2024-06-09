2ec1ec90c46d3120431f4660bbf2f189f1f9ff79
MrTinker64 <avi.micah@gmail.com>
2024-06-09 09:26:49 -0700

    removed another uneccessary debug line

--- time_adder.py
+++ time_adder.py
@@ -15,5 +15,4 @@ total_minutes = (int(hour1) * 60) + int(minute1) + (int(hour2) * 60) + int(minut
 remainder_minutes = total_minutes % 60
 total_hours = total_minutes // 60 % 12
 
-# print(f"Hour1 = {hour1}, Minute1 = {minute1}\nHour2 = {hour2}, Minute2 = {minute2}")
 print(f"New time: {total_hours}:{add_0(remainder_minutes)}")
