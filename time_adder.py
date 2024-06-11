e4ad5b58fd25061346a16e156019414a79a57d33
MrTinker64 <avi.micah@gmail.com>
2024-06-10 20:14:44 -0700

    added amp/pm support and defaulting to current time

--- time_adder.py
+++ time_adder.py
@@ -1,16 +1,36 @@
+from datetime import datetime
+
 def add_0(minutes):
     if minutes < 10:
         return "0" + str(minutes)
     else:
         return str(minutes)
 
-hour1, minute1 = input("time1: ").split(":")
-time2 = input("time2: ")
-try:
-    hour2, minute2 = time2.split(":")
-except ValueError:
-    hour2 = time2
-    minute2 = 0
+def get_time(index: int):
+    inputTime = input(f"time{index}: ")
+    if inputTime == "":
+        current_time = datetime.now().strftime('%H:%M')
+        inputTime = current_time
+    
+    try:
+        time, am_pm = inputTime.split(" ")
+    except ValueError:
+        am_pm = ""
+        time = inputTime
+        
+    try:
+        hour, minute = time.split(":")
+    except ValueError:
+        hour = time
+        minute = 0
+
+    if am_pm == "PM":
+        hour = int(hour) + 12
+        
+    return int(hour), int(minute)
+
+hour1, minute1 = get_time(1)
+hour2, minute2 = get_time(2)
 total_minutes = (int(hour1) * 60) + int(minute1) + (int(hour2) * 60) + int(minute2)
 remainder_minutes = total_minutes % 60
 total_hours = total_minutes // 60 % 12 or 12
